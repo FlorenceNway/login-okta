a9f7319bf662ad9c412080a47797fcd2c17bd5dd
Florence <florence.nway@hotmail.com>
2021-07-07 17:12:40 +0100

    Private route

---
+++ src/components/Private.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Private = () => {
+    return (
+        <div className='page'>
+            <div className='warning'>
+                <h1>Restriced Access</h1>
+                <h2>Authorized Personnel Only</h2>
+                <h3>You are authorized!!</h3>
+            </div>
+        </div>
+    );
+};
+
+export default Private;
